d89b4481daf304960d98336ca00c463e33cfbfce
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-02-15 19:20:32 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
       azurerm = {
       source  = "hashicorp/azurerm"
-<<<<<<< HEAD
-      version = "~3.62.1"
-=======
       version = "~=3.0.0"
->>>>>>> 93e4e876e43a90e75216f092358ba17565a9684a
     }
   }
 }
@@ -14,36 +10,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=3.0.0"
-#     }
-#   }
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
-=======
->>>>>>> 93e4e876e43a90e75216f092358ba17565a9684a

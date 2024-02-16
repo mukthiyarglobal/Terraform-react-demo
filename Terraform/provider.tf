3b3624b2a5fadbd95661947bf2d589ab7eccc283
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-02-16 16:13:43 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -9,8 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  # client_id       = "b386e414-666a-4efc-8fe2-dd3bf073e6ee"
-  # client_secret   = "gLq8Q~qZ~j24guUahXL4vuEA_TV5i5Dq8iQZhchZ"
-  # tenant_id       = "2db37aa6-01c8-4bea-814f-257fffdbfdb2"
-  # subscription_id = "6efc88ec-1dd6-40ed-bfa2-1c89fa0cd885"
+  
 }
